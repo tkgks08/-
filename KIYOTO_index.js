0a5d8f40e6f3973943c684a482fb6078fcbdc42f
高木啓助 <keisuke@takagikeisukes-MacBook-Air.local>
2022-03-07 10:38:43 +0900

    キヨトさん

---
+++ KIYOTO_index.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const ul = document.getElementById("ul");
+
+form.addEventListener("submit", function (event){
+    event.preventDefault();
+    console.log(input.value);
+    add();
+})
+
+function add() {
+    let todoText = input.value;
+    if (todoText){
+        const li = document.createElement("li");
+        li.innerText = todoText;
+        li.classList.add("list-group-item");
+        ul.appendChild(li);
+        input.value = "";
+        saveData();
+    }
+}
+
+function saveData () {
+    const lists = document.querySelectorAll("li");
+    console.log(lists);
+}
+
+
